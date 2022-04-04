37fc0b11efccfb6c29d5da3941a34ebdf31444fb
ابو عمير <aboamer0548@gmail.com>
2022-04-05 01:06:57 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("main-nav")[0];
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
