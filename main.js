b8ca7bde911b2ab13e0f26568915960d56ac6916
ابو عمير <aboamer0548@gmail.com>
2022-04-05 09:30:05 +0200

    update

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+function darkMode() {
+  var element = document.body;
+  element.className = "dark-mode";
+}
+function lightMode() {
+  var element = document.body;
+  element.className = "light-mode";
+}
 
+function myFunction() {
+    document.getElementById("clicklang").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.click')) {
+      var dropdowns = document.getElementsByClassName("menu-lang");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
