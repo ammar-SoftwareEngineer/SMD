d0471e48145373dc87fbcf09a12273f78b9dbeef
ابو عمير <aboamer0548@gmail.com>
2022-04-06 04:03:55 +0200

    update

--- main.js
+++ main.js
@@ -2,13 +2,8 @@ function darkMode() {
   var element = document.body;
   element.className = "dark-mode";
 }
+
 function lightMode() {
   var element = document.body;
   element.className = "light-mode";
-}
-
-
-function menu() {
-  var element = document.body;
-  element.className = "main-nav";
 }
